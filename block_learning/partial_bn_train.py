796bb98e206d509bbe98c5b18e06ba6d1dd1ce8e
Anaxagor Tesla <anaxagor.tesla@gmail.com>
2020-10-19 15:27:58 +0300

    Add K2 alg for structure learning

--- block_learning/partial_bn_train.py
+++ block_learning/partial_bn_train.py
@@ -1,7 +1,8 @@
 from pomegranate import BayesianNetwork
 from block_learning.train_bn import train_model
 import numpy as np
-
+from copy import copy
+import pandas as pd
 """
 Function for adding new BN and 
 trainig new connected BN with 
@@ -29,11 +30,14 @@ BN with partial training
 
 
 
-def partial_model_train(bn1: BayesianNetwork, data: np.ndarray, init_nodes: list = None, clusters: int = 5) -> BayesianNetwork:
+def partial_model_train(bn1: BayesianNetwork, data: pd.DataFrame, algorithm: str, init_nodes: list = None, clusters: int = 5) -> BayesianNetwork:
     
     hidden_input_var = np.array(bn1.marginal()[-1].sample(data.shape[0]))
-    new_data = np.column_stack((hidden_input_var, data))
-    bn = train_model(new_data, clusters = clusters, init_nodes = init_nodes)
+    new_data = pd.DataFrame()
+    new_data['hidden_input'] = hidden_input_var
+    new_data = pd.concat([new_data, data], axis=1)
+    #new_data = np.column_stack((hidden_input_var, data))
+    bn = train_model(new_data, algorithm = algorithm, clusters = clusters, init_nodes = init_nodes)
     return(bn)
     
 
--- block_learning/train_bn.py
+++ block_learning/train_bn.py
@@ -3,6 +3,11 @@ from pomegranate import BayesianNetwork, DiscreteDistribution
 from pyBN.classes.bayesnet import BayesNet
 from sklearn.cluster import KMeans
 import numpy as np
+from copy import copy
+import pandas as pd
+from pgmpy.estimators import HillClimbSearch
+from pgmpy.estimators import K2Score, BicScore
+from pgmpy.models import BayesianModel
 
 """
 Function for trainig the structure
@@ -21,6 +26,12 @@ the hidden var
 list of initial nodes which
 can't have parents
 
+-algorithm
+Algorithm for structure 
+learning
+-MI
+-K2
+
 
 Output:
 BayesianNetwork object
@@ -28,30 +39,59 @@ BayesianNetwork object
 
 """
 
-def train_model(data: np.ndarray, clusters: int = 5, init_nodes: list = None) -> BayesianNetwork:
+def train_model(data: pd.DataFrame, algorithm: str, clusters: int = 5, init_nodes: list = None) -> BayesianNetwork:
     
-    bn = BayesNet()
+    bn = BayesianNetwork()
     #Сluster the initial data in order to fill in a hidden variable based on the distribution of clusters
-    kmeans = KMeans(n_clusters = clusters, random_state = 0).fit(data)
+    kmeans = KMeans(n_clusters = clusters, random_state = 0).fit(data.values)
     labels = kmeans.labels_
     hidden_dist = DiscreteDistribution.from_samples(labels)
     hidden_var = np.array(hidden_dist.sample(data.shape[0]))
-    new_data = np.column_stack((data, hidden_var))
+
+    #new_data = np.column_stack((data, hidden_var))
+    new_data = copy(data)
+    new_data['hidden_output'] = hidden_var
     latent = (new_data.shape[1])-1
 
     #Train the network structure on data taking into account a hidden variable
-    bn = hc_rr(new_data, latent = latent, init_nodes = init_nodes)
-    structure = []
-    nodes = sorted(list(bn.nodes()))
-    for rv in nodes:
-        structure.append(tuple(bn.F[rv]['parents']))
-    structure = tuple(structure)
-    bn = BayesianNetwork.from_structure(new_data, structure)
-    bn.bake()
+    if algorithm == 'MI':
+        bn = hc_rr(new_data.values, latent = latent, init_nodes = init_nodes)
+        structure = []
+        nodes = sorted(list(bn.nodes()))
+        for rv in nodes:
+            structure.append(tuple(bn.F[rv]['parents']))
+        structure = tuple(structure)
+        bn = BayesianNetwork.from_structure(new_data, structure)
+        bn.bake()
+    if algorithm == 'K2':
+        datacol = new_data.columns.to_list()
+        if init_nodes: 
+            init_nodes_named = [new_data.columns.to_list()[i] for i in init_nodes]
+            blacklist = [(x, y) for x in datacol for y in init_nodes_named if x != y]
+        latent_list = [(datacol[latent], x) for x in datacol if x != datacol[latent]]
+        blacklist_total = list(set(blacklist + latent_list))
+        
+        hc_K2Score = HillClimbSearch(new_data, scoring_method=BicScore(new_data))
+        best_model_K2Score = hc_K2Score.estimate(black_list=blacklist_total)
+        structure = dict([(n, []) for n in new_data.columns])
+        for edge in best_model_K2Score.edges():
+            structure[edge[1]].append(edge[0])
+        column_dict = dict([(n, i) for i, n in enumerate(new_data.columns)])
+        structure2 = []
+        for n in new_data.columns:
+            l = structure[n]
+            l1 = []
+            for elem in l:
+                l1.append(column_dict[elem])
+            structure2.append(tuple(l1))
+        structure2 = tuple(structure2)
+        bn = BayesianNetwork.from_structure(new_data.values, structure2)
+        bn.bake()
     #Learn a hidden variable
+ 
     hidden_var = np.array([np.nan] * (data.shape[0]))
-    new_data = np.column_stack((data, hidden_var))
-    bn.predict(new_data)
+    new_data = np.column_stack((data.values, hidden_var))
+    new_data = bn.predict(new_data)
     bn.fit(new_data)
     bn.bake()
     return (bn)
