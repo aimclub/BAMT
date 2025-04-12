ed59ed1cac277c9023284298ae4e69531ce9e43f
Артём Кузнецов <kmetra1910@gmail.com>
2025-04-12 13:41:17 +0300

    initialisation to be as it is in gmr but using only sklearn mixture package

--- bamt/utils/gmm_wrapper.py
+++ bamt/utils/gmm_wrapper.py
@@ -1,4 +1,12 @@
 import numpy as np
 from sklearn.mixture import GaussianMixture
 
+class GMM:
+    def __init__(self, n_components = 1, priors = None, means = None, covariances = None, random_state = None):
+        self.n_components = n_components
+        self.random_state = random_state
+        self._gmm = None  # объект GaussianMixture или ручная сборка
+
+        if means is not None and covariances is not None and priors is not None:
+            self._manual_init(means, covariances, priors)
 
